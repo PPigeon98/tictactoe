47e131d43f2e1802b3af0c473a183f5f47b9b68d
Brian Wibawa Adi <z5585139@ad.unsw.edu.au>
2025-09-03 22:29:01 +1000

    drawing

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,39 @@
 #include <iostream>
 
+char board[3][3];
+const char p1 = 'X';
+const char p2 = 'O';
+bool turn = 0; // 0 = p1, 1 = p2
+
+void init() {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      board[i][j] = ' ';
+    }
+  }
+}
+
+void draw() {
+  std::cout << "Player " << (turn ? "2" : "1") << "'s turn:\n";
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (j != 2) {
+        std::cout << ' ' << board[i][j] << " │";
+      } else {
+        std::cout << ' ' << board[i][j];
+      }
+    }
+    if (i != 2) {
+      std::cout << "\n───┼───┼───\n";
+    } else {
+      std::cout << '\n';
+    }
+  }
+}
+
 int main() {
-  std::cout << "hi\n";
+  init();
+  draw();
 
   return 0;
 }
