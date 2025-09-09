267c76ad2547489471334690421cefc2255caa67
Brian Wibawa Adi <z5585139@ad.unsw.edu.au>
2025-09-09 12:34:54 +1000

    size

--- src/tictactoe.cc
+++ src/tictactoe.cc
@@ -1,11 +1,6 @@
 #include <ncurses.h>
 #include <locale.h>
 
-// TODO:
-// have bigger boards
-// make cursor thick
-// optimise ugly code
-
 char board[3][3];
 const char P1_ = 'X';
 const char P2_ = 'O';
@@ -15,31 +10,56 @@ int y = 1;
 int width = 3;
 int height = 1;
 
-// row1, row2, row3, col1, col2, col3, diagnegative, diagpositive
-int p1[8];
-int p2[8];
-
 void init() {
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       board[i][j] = ' ';
     }
   }
-  move(y * (height + 1) + height / 2 + 1, 
-       x * (width + 1) + width / 2);
+  move(y * (height + 1) + height / 2 + 1, x * (width + 1) + width / 2);
 }
 
 void draw() {
   move(0, 0);
   turn ? printw("Player 2's turn:\n") : printw("Player 1's turn:\n");
   for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < height / 2; j++) {
+      for (int l = 0; l < 3; l++) {
+        for (int k = 0; k < width; k++) {
+          printw(" ");
+        }
+        if (l != 2) {
+          printw("│");
+        }
+      }
+      printw("\n");
+    }
+
     for (int j = 0; j < 3; j++) {
+      for (int k = 0; k < (width - 1) / 2; k++) {
+        printw(" ");
+      }
+      printw("%c", board[i][j]);
+      for (int k = 0; k < width / 2; k++) {
+        printw(" ");
+      }
       if (j != 2) {
-        printw(" %c │", board[i][j]);
-      } else {
-        printw(" %c", board[i][j]);
+        printw("│");
       }
     }
+
+    for (int j = 0; j < height / 2; j++) {
+      printw("\n");
+      for (int l = 0; l < 3; l++) {
+        for (int k = 0; k < width; k++) {
+          printw(" ");
+        }
+        if (l != 2) {
+          printw("│");
+        }
+      }
+    }
+
     if (i != 2) {
       printw("\n");
       for (int j = 0; j < 3; j++) {
@@ -55,32 +75,7 @@ void draw() {
       printw("\n");
     }
   }
-  move(y * (height + 1) + height / 2 + 1, 
-       x * (width + 1) + width / 2);
-}
-
-void update() {
-  if (turn) {
-    board[y][x] = P2_;
-    p2[y + 3]++;
-    p2[x]++;
-    if (x == y) {
-      p2[6]++;
-    }
-    if (x + y == 2) {
-      p2[7]++;
-    }
-  } else {
-    board[y][x] = P1_;
-    p1[y + 3]++;
-    p1[x]++;
-    if (x == y) {
-      p1[6]++;
-    }
-    if (x + y == 2) {
-      p1[7]++;
-    }
-  }
+  move(y * (height + 1) + height / 2 + 1, x * (width + 1) + width / 2);
 }
 
 void select() {
@@ -92,42 +87,42 @@ void select() {
         if (y > 0) {
           y--;
         }
-        move(y * (height + 1) + height / 2 + 1, 
-            x * (width + 1) + width / 2);
+        move(y * (height + 1) + height / 2 + 1, x * (width + 1) + width / 2);
         break;
       case 66:    // key down
         if (y < 2) {
           y++;
         }
-        move(y * (height + 1) + height / 2 + 1, 
-            x * (width + 1) + width / 2);
+        move(y * (height + 1) + height / 2 + 1, x * (width + 1) + width / 2);
         break;
       case 67:    // key right
         if (x < 2) {
           x++;
         }
-        move(y * (height + 1) + height / 2 + 1, 
-            x * (width + 1) + width / 2);
+        move(y * (height + 1) + height / 2 + 1, x * (width + 1) + width / 2);
         break;
       case 68:    // key left
         if (x > 0) {
           x--;
         }
-        move(y * (height + 1) + height / 2 + 1, 
-            x * (width + 1) + width / 2);
+        move(y * (height + 1) + height / 2 + 1, x * (width + 1) + width / 2);
         break;
       case '\n':  // return
         if (board[y][x] == ' ') ch = -2;
-        update();
+        turn ? board[y][x] = P2_ : board[y][x] = P1_;
         break;
-      case '1':
-        width = 3;
-        height = 1;
-        draw();
+      case '-':
+        if (width > 3 && height > 1) {
+          width -= 5;
+          height -= 2;
+          clear();
+          draw();
+        }
         break;
-      case '2':
-        width = 5;
-        height = 2;
+      case '+':
+        width += 5;
+        height += 2;
+        clear();
         draw();
         break;
     }
@@ -135,13 +130,24 @@ void select() {
 }
 
 int win() {
-  for (int i = 0; i < 8; i++) {
-    if (p1[i] == 3) {
-      return 1;
+  for (int i = 0; i < 3; i++) {
+    if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+      return (board[i][0] == P1_) ? 1 : 2;
     }
-    if (p2[i] == 3) {
-      return 2;
+  }
+  
+  for (int j = 0; j < 3; j++) {
+    if (board[0][j] != ' ' && board[0][j] == board[1][j] && board[1][j] == board[2][j]) {
+      return (board[0][j] == P1_) ? 1 : 2;
     }
+  }
+  
+  if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+    return (board[0][0] == P1_) ? 1 : 2;
+  }
+  
+  if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+    return (board[0][2] == P1_) ? 1 : 2;
   }
 
   for (int i = 0; i < 3; i++) {
@@ -162,7 +168,7 @@ void gameloop() {
 
     int result = win();
     if (result > 0) {
-      move(10, 0);
+      move(0, 0);
       if (result == 1) {
         printw("Game Over! Player 1 wins! Press any key to exit.");
       } else if (result == 2) {
@@ -183,6 +189,7 @@ void gameloop() {
 int main() {
   setlocale(LC_ALL, ""); // Generate locales if not working
   initscr();
+  curs_set(2);
   noecho();
 
   init();
